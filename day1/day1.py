f8743d4ba79ac2130fe1a0c2c1afcb914ea6a46a
Brent Min <brentmin@gmail.com>
2025-12-08 14:47:25 -0800

    day one

---
+++ day1/day1.py
@@ -0,0 +1,30 @@
+# load in text data from file
+with open('day1.txt', 'r') as file:
+    data = file.read()
+
+# split the data into a list of numbers
+numbers = data.split()
+
+current = 50
+answer = 0
+50 - 51 == 99
+
+for number in numbers:
+
+	num = int(number[1:]) % 100
+	if number[0] == 'L':
+		if current - num < 0:
+			current = 100 - (num - current)
+		else:
+			current -= num
+	if number[0] == 'R':
+		if current + num > 99:
+			current = current + num - 100
+		else:
+			current += num
+
+	print(current)
+	if current == 0:
+		answer += 1
+
+print(answer)
